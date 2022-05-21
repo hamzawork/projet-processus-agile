bda2659a121b2357b45b4deaf1716a5862242642
saadzobid <105944463+saadzobid@users.noreply.github.com>
2022-05-21 13:40:20 +0100

    Add files via upload

---
+++ Project/Login1/acceuil.php
@@ -0,0 +1,71 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PFA</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<header>
+		
+		
+		<div class="main">
+			<div class="tache">
+				<ul class="um5">
+					<img src="image/1.png">
+				</ul>
+				<ul class="ensias">
+					<img src="image/3.png">
+				</ul>
+				<div class="background"></div>
+			</div>
+			
+			<div class="logo">
+				<ul class="EN">
+					<img src="image/logo1.png">
+				</ul>
+					
+					<label for="show"> 
+						<a href="login-user.php"> 
+						<ul class="menu2">
+							<img  src="image/etude.png" alt="form">
+							<div class="form_overlay">
+								<div  class="form_titre">Etudiant</div>
+								<p class="form_description">
+								Espace des eleves ingenieurs de l'ENSIAS.
+								</p>
+							</div>
+						</ul>
+					</a>
+					</label>
+				<label for="show"> 
+					<a href="login-prof.php">
+					<ul class="menu3">
+						<img src="image/prof.png">
+						<div class="site_overlay">
+							<div class="site_titre">Professeur</div>
+							<p class="site_description">
+								Espace des professeurs et enseignants de l'ENSIAS
+							</p>
+						</div>
+					</ul>
+				</label>
+				<label for="show">
+					<a href="login-admin.php">
+					<ul class="menu4">
+						<img src="image/admin.png">
+						<div class="cours_overlay">
+							<div class="cours_titre">Administration</div>
+							<p class="cours_description">
+								Espace des services administartifs de l'ENSIAS
+							</p>
+						</div>
+					</ul>
+				</label>
+			</div>
+		</div>
+	</header>
+</body>
+</html>
